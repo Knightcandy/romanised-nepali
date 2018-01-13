38622d91bb55092da44f5b579b42057d328adbd4
sujan poudel <spoudel347@gmail.com>
2018-01-13 19:52:56 +0545

    convert function exported

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 import http from 'http';
 import * as qs from querystring ;
 import {translate} from './core';
+const PORT =8058;
+function startServer()
+{
+    http.createServer(function (req, res) {
 
-http.createServer(function (req, res) {
-
-  if (req.method === 'POST') {
-    var body = '';
-    req.on('data', function(chunk) {
-      body += chunk;
-    });
-    req.on('end', function() {
-      var data = qs.parse(body);
-      res.writeHead(200);
-      var converted=translate(data.data,data.smartConvert);
-      res.end(converted);
-    });
-  } else {
-    res.writeHead(404);
-    res.end();
-  }
-
-}).listen(1234);
+        if (req.method === 'POST') {
+          let body = '';
+          req.on('data', function(chunk) {
+            body += chunk;
+          });
+          req.on('end', function() {
+            var data = qs.parse(body);
+            res.writeHead(200);
+            var converted=translate(data.data,data.smartConvert);
+            res.end(converted);
+          });
+        } else {
+          res.writeHead(404);
+          res.end();
+        }
+      }).listen(PORT);
+}
+export function convert(raw,smartConvert)
+{
+    return translate(raw,smartConvert);
+}
