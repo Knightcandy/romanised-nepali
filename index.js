ccb0cc7333f3b316dedc7f4abfdf069b0a8f8321
sujan poudel <spoudel347@gmail.com>
2018-01-14 11:08:17 +0545

    import is replaced with require

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-#!/usr/bin/node 
-import http from 'http';
-import * as qs from querystring ;
-import {translate} from './core';
+#!/usr/bin/node
+var core = require("./core.js");
+var http = require('http');
+var qs = require('querystring');
+
 const PORT =8058;
 function startServer()
 {
@@ -15,12 +16,8 @@ function startServer()
           req.on('end', function() {
             var data = qs.parse(body);
             res.writeHead(200);
-            var converted=translate(data.data,data.smartConvert);
+            var converted=core.translate(data.data,data.smartConvert);
             res.end(converted);
-            //  console.log("raw input:"+data.data);
-            //  console.log("converting with smartConvert="+data.smartConvert);
-            // console.log("send after converted"+converted+"|");
-
           });
         } else {
           res.writeHead(404);
@@ -28,7 +25,7 @@ function startServer()
         }
       }).listen(PORT);
 }
-export function convert(raw,smartConvert)
+function convert(raw,smartConvert)
 {
     return translate(raw,smartConvert);
 }
